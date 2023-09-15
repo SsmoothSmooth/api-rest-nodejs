380a3bfce79a2c2dacb8ca030fb766e286e3c952
José Wilson de Lima <josewilson791@gmail.com>
2023-09-15 10:25:55 -0300

    API-rest-nodejs

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import { app } from './app'
+import { env } from './env'
+
+app
+  .listen({
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('HTTP server Running on http//:localhost:3333')
+  })
